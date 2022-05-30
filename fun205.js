fe0567c1788acb12005e632aa1a48acd70dfbaad
kawinnat09 <Kwinnathmayura@gmail.com>
2022-05-30 17:27:06 +0700

    update

--- fun205.js
+++ fun205.js
@@ -14,6 +14,10 @@ function div(x,y){
     return x/y
 }
 
+function liv(x,y){
+    return x-y
+}
+
 function calculate(callback,a,b){
     return `${callback(a,b)}`
 }
@@ -22,8 +26,10 @@ let x = calculate(add,2,1)
 let y = calculate(sub,2,1)
 let z = calculate(mul,2,1)
 let a = calculate(div,2,1)
+let champ = calculate(liv,2,1)
 
 console.log(`add is = x+y =${x}`)
 console.log(`sub is = x-y =${y}`)
 console.log(`mul is = x*y =${z}`)
 console.log(`div is = x/y =${a}`)
+console.log(`liv is = x-y =${champ}`)
