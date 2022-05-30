74b6d59f5b6381d77dfe4c5765c60e33fb6feb8c
kawinnat09 <Kwinnathmayura@gmail.com>
2022-05-30 15:06:17 +0700

    update

---
+++ switch.js
@@ -0,0 +1,40 @@
+var day = 5
+switch(day){
+    case 0:{
+        console.log("Sunday")
+        break;
+    }
+
+    case 1:{
+        console.log("Monday")
+        break;
+    }
+
+    case 2:{
+        console.log("Tuseday")
+        break;
+    }
+
+    case 3:{
+        console.log("Wednesday")
+        break;
+    }
+
+    case 4:{
+        console.log("Thurday")
+        break;
+    }
+
+    case 5:{
+        console.log("Friday")
+        break;
+    }
+
+    case 6:{
+        console.log("Saturday")
+        break;
+    }
+    default:{
+        console.log("ERROR")
+    }
+}
