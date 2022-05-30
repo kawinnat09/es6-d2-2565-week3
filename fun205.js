35d2d13c5f565cafc7298ba86f7b1968c4a5dc9c
kawinnat09 <Kwinnathmayura@gmail.com>
2022-05-30 17:21:19 +0700

    update

---
+++ fun205.js
@@ -0,0 +1,23 @@
+function add(x,y){
+    return x+y
+}
+
+function sub(x,y){
+    return x-y
+}
+
+function mul(x,y){
+    return x*y
+}
+
+function calculate(callback,a,b){
+    return `${callback(a,b)}`
+}
+
+let x = calculate(add,2,1)
+let y = calculate(sub,2,1)
+let z = calculate(mul,2,1)
+
+console.log(`add is = x+y =${x}`)
+console.log(`sub is = x-y =${y}`)
+console.log(`mul is = x*y =${z}`)
